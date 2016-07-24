afb42ab97f884434bcf64dfd44a6515a174039ee
Toli Carter <drachis@gmail.com>
2016-07-23 17:40:00 -0700

    rise and fall of numbers if timed out and non blocking
    '

--- 2016/HighStriker/HighStriker.ino
+++ 2016/HighStriker/HighStriker.ino
@@ -1,46 +1,46 @@
 /* SFE_BMP180 altitude example sketch
 
-This sketch shows how to use the Bosch BMP180 pressure sensor
-as an altimiter.
-https://www.sparkfun.com/products/11824
+  This sketch shows how to use the Bosch BMP180 pressure sensor
+  as an altimiter.
+  https://www.sparkfun.com/products/11824
 
-Like most pressure sensors, the BMP180 measures absolute pressure.
-Since absolute pressure varies with altitude, you can use the pressure
-to determine your altitude.
+  Like most pressure sensors, the BMP180 measures absolute pressure.
+  Since absolute pressure varies with altitude, you can use the pressure
+  to determine your altitude.
 
-Because pressure also varies with weather, you must first take a pressure
-reading at a known baseline altitude. Then you can measure variations
-from that pressure
+  Because pressure also varies with weather, you must first take a pressure
+  reading at a known baseline altitude. Then you can measure variations
+  from that pressure
 
-Hardware connections:
+  Hardware connections:
 
-- (GND) to GND
-+ (VDD) to 3.3V
+  - (GND) to GND
+  + (VDD) to 3.3V
 
-(WARNING: do not connect + to 5V or the sensor will be damaged!)
+  (WARNING: do not connect + to 5V or the sensor will be damaged!)
 
-You will also need to connect the I2C pins (SCL and SDA) to your
-Arduino. The pins are different on different Arduinos:
+  You will also need to connect the I2C pins (SCL and SDA) to your
+  Arduino. The pins are different on different Arduinos:
 
-Any Arduino pins labeled:  SDA  SCL
-Uno, Redboard, Pro:        A4   A5
-Mega2560, Due:             20   21
-Leonardo:                   2    3
+  Any Arduino pins labeled:  SDA  SCL
+  Uno, Redboard, Pro:        A4   A5
+  Mega2560, Due:             20   21
+  Leonardo:                   2    3
 
-Leave the IO (VDDIO) pin unconnected. This pin is for connecting
-the BMP180 to systems with lower logic levels such as 1.8V
+  Leave the IO (VDDIO) pin unconnected. This pin is for connecting
+  the BMP180 to systems with lower logic levels such as 1.8V
 
-Have fun! -Your friends at SparkFun.
-   
-The SFE_BMP180 library uses floating-point equations developed by the
-Weather Station Data Logger project: http://wmrx00.sourceforge.net/
+  Have fun! -Your friends at SparkFun.
 
-Our example code uses the "beerware" license. You can do anything
-you like with this code. No really, anything. If you find it useful,
-buy me a beer someday.
+  The SFE_BMP180 library uses floating-point equations developed by the
+  Weather Station Data Logger project: http://wmrx00.sourceforge.net/
 
-V10 Mike Grusin, SparkFun Electronics 10/24/2013
-V1.1.2 Updates for Arduino 1.6.4 5/2015
+  Our example code uses the "beerware" license. You can do anything
+  you like with this code. No really, anything. If you find it useful,
+  buy me a beer someday.
+
+  V10 Mike Grusin, SparkFun Electronics 10/24/2013
+  V1.1.2 Updates for Arduino 1.6.4 5/2015
 */
 
 // Your sketch must #include this library, and the Wire library.
@@ -57,7 +57,7 @@ double baseline; // baseline pressure
 
 void setup()
 {
-  pinMode(13,OUTPUT);
+  pinMode(13, OUTPUT);
   Serial.begin(9600);
   Serial.println("REBOOT");
 
@@ -71,22 +71,22 @@ void setup()
     // see the comments at the top of this sketch for the proper connections.
 
     Serial.println("BMP180 init fail (disconnected?)\n\n");
-    while(1); // Pause forever.
+    while (1); // Pause forever.
   }
 
   // Get the baseline pressure:
-  
+
   baseline = getPressure();
-  
+
   Serial.print("baseline pressure: ");
   Serial.print(baseline);
-  Serial.println(" mb");  
+  Serial.println(" mb");
 }
 
 void loop()
 {
-  double a,P,b;
-  
+  double a, P, b;
+
   // Get a new pressure reading:
 
   P = getPressure();
@@ -94,43 +94,43 @@ void loop()
   // Show the relative altitude difference between
   // the new reading and the baseline reading:
 
-  a = pressure.altitude(P,baseline);
-  
+  a = pressure.altitude(P, baseline);
+
   //Serial.print("relative altitude: ");
   //if (a >= 0.0) Serial.print(" "); // add a space for positive numbers
   //Serial.print(a,1);
   //Serial.print(" meters, ");
   //if (a >= 0.0) Serial.print(" "); // add a space for positive numbers
-  if(a<0){
-    digitalWrite(13,HIGH);
-  } else{
-    digitalWrite(13,LOW);
+  if (a < 0) {
+    digitalWrite(13, HIGH);
+  } else {
+    digitalWrite(13, LOW);
   }
-  
-  b = lightLevels(a,0.5,-1);
-  analogWrite(13,b);
+
+  b = lightLevels(a, 0.5, -1);
+  analogWrite(13, b);
   //Serial.println(b);
   delay(30);
 }
 
-double lightLevels(double value,int _min,int _max){
+double lightLevels(double value, int _min, int _max) {
   //returns a value between 1 and 255 in steps of 32
   int steps = 32;
   int range = 256;
-  double numerator = value-(double)_min;
-  double denominator = (double)(_max-_min);
-  int stepped = (numerator/denominator)*(8);
-    Serial.println(stepped);
+  double numerator = value - (double)_min;
+  double denominator = (double)(_max - _min);
+  int stepped = (numerator / denominator) * (8);
+  Serial.println(stepped);
   return abs(stepped);
 }
 
 double getPressure()
 {
   char status;
-  double T,P,p0,a;
+  double T, P, p0, a;
 
   // You must first get a temperature measurement to perform a pressure reading.
-  
+
   // Start a temperature measurement:
   // If request is successful, the number of ms to wait is returned.
   // If request is unsuccessful, 0 is returned.
@@ -168,10 +168,10 @@ double getPressure()
         // (If temperature is stable, you can do one temperature measurement for a number of pressure measurements.)
         // Function returns 1 if successful, 0 if failure.
 
-        status = pressure.getPressure(P,T);
+        status = pressure.getPressure(P, T);
         if (status != 0)
         {
-          return(P);
+          return (P);
         }
         else Serial.println("error retrieving pressure measurement\n");
       }

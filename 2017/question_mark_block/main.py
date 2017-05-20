84587a49970efe99fb4e459cf03d1c4366ec63f3
Toli Carter <toli@gmail.com>
2017-05-20 01:15:47 -0700

    desired functionality inplace, timing might need work

--- 2017/question_mark_block/main.py
+++ 2017/question_mark_block/main.py
@@ -1,9 +1,15 @@
 import sys, os, pygame, random, time
 
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
-pin = 17
-GPIO.setup(pin,GPIO.IN,GPIO.PUD_UP)
+try:
+	import RPi.GPIO as GPIO
+	GPIO.setmode(GPIO.BCM)
+	hasGPIO = True
+	pin = 17
+	GPIO.setup(pin,GPIO.IN,GPIO.PUD_UP)
+except:
+	print("unable to find GPIO, stepping over")
+	pin = "DUMMY"
+	hasGPIO = False
 prev = False
 
 def findSprites(directory, ext = [".gif", ".png", ".GIF", ".PNG"]):
@@ -16,12 +22,30 @@ def findSprites(directory, ext = [".gif", ".png", ".GIF", ".PNG"]):
 				print (sprites[-1])
 	return sprites
 
+def getRandomSprite(sprites):
+	#make more powerful sprites a bit rarer
+	rand = random.random()
+	idx = 0
+	if rand < 0.30:
+		idx = 0
+	elif rand < 0.55:
+		idx = 1
+	elif rand < 0.75:
+		idx = 2
+	elif rand < 0.9:
+		idx = 3
+	elif rand < 0.95:
+		idx = 4
+	else:
+		idx = 5 
+	print (idx)
+	return sprites[idx]
+
 def incrementSprite(spriterect, sprites):
 	scale = 8*4
 	x,y,u,v = spriterect
-	idx = random.randint(0,len(sprites)-1)
-	s = sprites[idx]
-	print(s)
+	s = getRandomSprite(sprites)
+	#print(s)
 	sprite = pygame.image.load(s)
 	sprite = pygame.transform.scale(sprite, (int(sprite.get_size()[0])*scale,int(sprite.get_size()[1])*scale))
 	spriterect = sprite.get_rect()
@@ -55,20 +79,26 @@ on button press / input
 	Reactivate after 10-30s as a question mark [?] 
 		- this is to prevent people from spamming hits.
 		- make some sort of power-up available (yoshi mount?) sound 
+
 '''
-	
 def pygameMain():
 	pygame.init()
 	clock = pygame.time.Clock()
 	size = width, height = 800, 600
 	speed = [8,8]
 	black = 0, 0, 0
+	white = 255, 255, 255
 
 	screen = pygame.display.set_mode(size)
 	sprites = findSprites("./sprites/")
 	idx = random.randint(0,len(sprites)-1)
 	s = sprites[idx]
 	sprite = pygame.image.load(s)
+	questionMark = pygame.image.load("questonmark_front.png")
+	questionMark = pygame.transform.scale(questionMark,(int(size[0]*1.2), int(size[0]*1.2)))
+	questionRect = questionMark.get_rect()
+	questionRect.x = size[0]/2-questionRect[2]/2
+	questionRect.y = size[1]/2-questionRect[3]/2
 	spriterect = sprite.get_rect()
 	sprite, spriterect = incrementSprite(spriterect,sprites)
 	pygame.display.toggle_fullscreen()
@@ -78,37 +108,44 @@ def pygameMain():
 	pygame.event.set_allowed(pygame.QUIT)
 	pygame.event.set_allowed(pygame.KEYUP)
 	prev = False
+	button = True
+	swapped = time.time()
+	hold = 8
+	ready = True
 	while True:
-		#pygame.time.wait(150)
-		#pygame.event.wait()
-		time.sleep(0.1)
 		for event in pygame.event.get():
 			print(event)
-			if (event.type== pygame.KEYUP) or (event.type == pygame.QUIT) :
+			if (event.type == pygame.KEYUP and event.key == 27) or (event.type == pygame.QUIT) :
 				pygame.quit()
 				sys.exit()
-		button = GPIO.input(pin)
-		if button != prev: 
+			if not hasGPIO and event.type == pygame.KEYUP:
+				if event.key == 32:
+					button = True
+				else: 
+					button = False
+		if hasGPIO:
+			button = GPIO.input(pin)
+		if button != prev and ready: 
+			print (button)
 			if button:
 				print (pin , ", open")
 			else:
 				sprite, spriterect = incrementSprite(spriterect, sprites)
 				print(pin , ", pressed")
+				swapped = time.time()
 			prev = button
-		"""spriterect = spriterect.move(speed)
-		if spriterect.left < 0 or spriterect.right > width:
-			speed[0] = -speed[0]
-			sprite, spriterect = incrementSprite(spriterect, sprites)
-		if spriterect.top < 0 or spriterect.bottom > height:
-			speed[1] = -speed[1]
-			sprite, spriterect = incrementSprite(spriterect, sprites)
-		"""
-		screen.fill(black)
-		screen.blit(sprite, spriterect)
-		pygame.display.flip()
-		#ms = clock.tick(60)
-		#if (ms < 1000./60):
-			#pygame.time.wait(int(1000./60-ms))
-		#clock.tick()
+		spriterect.x = size[0]/2 - sprite.get_size()[0]/2 
+		spriterect.y =  size[1]/2 - sprite.get_size()[1]/2
+		if swapped + hold > time.time():
+			screen.fill(black)
+			screen.blit(sprite, spriterect)
+			pygame.display.flip()
+			ready = False
+		else:
+			ready = True
+			screen.fill(white)
+			screen.blit(questionMark, questionRect)
+			pygame.display.flip()			
+		time.sleep(0.1)
 if __name__ == "__main__":
 	pygameMain()
